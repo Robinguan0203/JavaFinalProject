174ee4c2a1952cfa99dfa779720da389fc6f3563
YAOZHOU, XIE <xie00087@algonquinlive.com>
2024-08-05 11:21:19 -0400

    Add UserFactoryTest

---
+++ src/test/java/com/fwrp/dbService/UserFactoryTest.java
@@ -0,0 +1,41 @@
+package com.fwrp.dbService;
+
+import com.fwrp.constants.UserTypeConstant;
+import com.fwrp.models.User;
+import com.fwrp.models.Retailer;
+import com.fwrp.models.Consumer;
+import com.fwrp.models.Charity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+ *
+ * @author YAOZHOU XIE
+ */
+public class UserFactoryTest {
+
+    @Test
+    public void testCreateRetailer() {
+        User user = UserFactory.createUser(UserTypeConstant.RETAILER);
+        assertTrue(user instanceof Retailer);
+    }
+
+    @Test
+    public void testCreateConsumer() {
+        User user = UserFactory.createUser(UserTypeConstant.CONSUMER);
+        assertTrue(user instanceof Consumer);
+    }
+
+    @Test
+    public void testCreateCharity() {
+        User user = UserFactory.createUser(UserTypeConstant.CHARITY);
+        assertTrue(user instanceof Charity);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateInvalidUserType() {
+        UserFactory.createUser(-1);
+    }
+}
